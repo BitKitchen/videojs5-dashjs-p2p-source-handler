b3006022465d3a4bafbae113096435dc637fb674
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2017-03-02 16:03:45 +0400

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const { version } = require('./package.json');
+
+module.exports = {
+    entry: './src/js/videojs-dash.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'videojs5-dashjs-p2p-source-handler.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
+        noParse: [
+            /node_modules\/streamroot-dashjs-p2p-bundle\/dashjs-p2p-bundle.js/,
+            /node_modules\/streamroot-p2p\/p2p.js/,
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VERSION__: JSON.stringify(`v${version}`)
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(true),
+        // commented out UglifyJsPlugin because it freezes build at `91% additional asset processing`
+        // new webpack.optimize.UglifyJsPlugin(),
+    ]
+};
